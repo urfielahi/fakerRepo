a350f515b45c01b864d93a42a7b58bf2af2b69bf
Urfeena <urfeena@preksh.com>
2017-11-27 14:54:02 +0530

    sql_driver_added

--- api/oldapp.js
+++ api/oldapp.js
@@ -1,72 +1,72 @@
-// const express = require('express')
+const express = require('express')
 
-// var MongoClient = require('mongodb').MongoClient;
-// var assert = require('assert');
-// var ObjectId = require('mongodb').ObjectID;
+var MongoClient = require('mongodb').MongoClient;
+var assert = require('assert');
+var ObjectId = require('mongodb').ObjectID;
 
-// const app = express();
-
-
-// var url = 'mongodb://localhost:27017/mongotest';
-// var service = require('./services/user.service')
+const app = express();
 
 
-// //create Fake User call
-// app.get('/create', (req, res) => {
-//   User = service.createNewUser();
-//   console.log("New User Created is : ")
-//   console.log(User)
-
-//   //Insert Call
-//   insertedUser = MongoClient.connect(url, function(err, db) {
-//       assert.equal(null, err);
-//       service.insertUser(db, User, function() {
-//         console.log("Inserted a document into the tests collection.");
-//         console.log(insertedUser);
-//         db.close();
-//       });
-//     });
-
-// })
-
-// app.get('/users', (req, res) => {
-//   users = MongoClient.connect(url, function(err, db) {
-//     assert.equal(null, err);
-//     service.getUsers(db, function() {
-//         db.close();
-//     });
-//    });
-//    res.send(users);
-// })
+var url = 'mongodb://localhost:27017/mongotest';
+var service = require('./services/user.service')
 
 
+//create Fake User call
+app.get('/create', (req, res) => {
+  User = service.createNewUser();
+  console.log("New User Created is : ")
+  console.log(User)
+
+  //Insert Call
+  insertedUser = MongoClient.connect(url, function(err, db) {
+      assert.equal(null, err);
+      service.insertUser(db, User, function() {
+        console.log("Inserted a document into the tests collection.");
+        console.log(insertedUser);
+        db.close();
+      });
+    });
+
+})
+
+app.get('/users', (req, res) => {
+  users = MongoClient.connect(url, function(err, db) {
+    assert.equal(null, err);
+    service.getUsers(db, function() {
+        db.close();
+    });
+   });
+   res.send(users);
+})
 
 
 
-// // //Insert function call
+
+
+// //Insert function call
 
 
 
-// // //Select function
-// // var findUsers = function(db, callback) {
-// //   var cursor =db.collection('tests').find( );
-// //   cursor.each(function(err, doc) {
-// //      assert.equal(err, null);
-// //      if (doc != null) {
-// //         console.dir(doc);
-// //      } else {
-// //         callback();
-// //      }
-// //   });
-// // };
+// //Select function
+// var findUsers = function(db, callback) {
+//   var cursor =db.collection('tests').find( );
+//   cursor.each(function(err, doc) {
+//      assert.equal(err, null);
+//      if (doc != null) {
+//         console.dir(doc);
+//      } else {
+//         callback();
+//      }
+//   });
+// };
 
-// // //Call the findUsers function.
+// //Call the findUsers function.
 
-// // MongoClient.connect(url, function(err, db) {
-// //  assert.equal(null, err);
-// //  findUsers(db, function() {
-// //      db.close();
-// //  });
-// // });
+// MongoClient.connect(url, function(err, db) {
+//  assert.equal(null, err);
+//  findUsers(db, function() {
+//      db.close();
+//  });
+// });
 
-// app.listen(3000, () => console.log('Example app listening on port 3000!'))
+app.listen(3000, () => console.log('Example app listening on port 3000!'))
--- app.js
+++ app.js
@@ -1,71 +1,136 @@
-const express = require('express')
-const faker = require('faker')
-
-//Mongo Connection
-var MongoClient = require('mongodb').MongoClient;
-var assert = require('assert');
-var ObjectId = require('mongodb').ObjectID;
-const url = 'mongodb://localhost:27017/mongotest';
-
+const express = require('express');
+const faker = require('faker');
 const app = express();
 
-//Select function
-var findUsers = function(db, callback) {
-  var cursor =db.collection('tests').find( );
-  cursor.each(function(err, doc) {
-     assert.equal(err, null);
-     if (doc != null) {
-        console.dir(doc);
-     } else {
-        callback();
-     }
-  });
-};//select close
+//------------------------------------------------------------------
+//  @MYSQL DB
+//---------------------------------------------------------------------
 
-
-//Create  function
-createNewUser = function(){
-    var User = {
-                name: faker.name.findName(),
-                email: faker.internet.email(),
-                website: faker.internet.url(),
-                address: faker.address.streetAddress() + faker.address.city() + faker.address.country(),
-                bio: faker.lorem.sentences(),
-                image: faker.image.avatar()
-        
-            }
-    console.log("User Created!!");
-  
-    //Insert function
-    MongoClient.connect(url, function(err, db) {
-      assert.equal(null, err);
-      db.collection('tests').insertOne( User, function(err, result) {
-        assert.equal(err, null);
-        console.log("Inserted a document into the tests collection.");
-        db.close();
-      });
-    });//insert close
-
-}//create close
-
-
-//Create Route
-app.get('/create', (req,res) => {
-   for(var i=1; i<=20; i++)
-   {
-    createNewUser();
-   }
-})
-
-//select Route
-app.get('/users', (req, res) => {
-  MongoClient.connect(url, function(err, db) {
- assert.equal(null, err);
- findUsers(db, function() {
-     db.close();
- });
+var mysql      = require('mysql');
+var connection = mysql.createConnection({
+    host     : 'localhost',
+    user     : 'root',
+    password : '',
+    database : 'faker'
 });
-})
+   
+    
+connection.connect(function(err){
+if(!err) {
+    console.log("Database is connected");    
+} else {
+    console.log("Error connecting database");    
+}
+});
+    
+connection.query('SELECT * FROM employees', (err,rows) => {
+    if(err) throw err;
+  
+    console.log('Data received from Db:\n');
+    rows.forEach( (row) => { 
+        console.log(`${row.name} is in ${row.location}`); 
+    });
+});
+
+for(var i=0; i< 100; i++){
+    var passenger = {
+                p_id: faker.random.uuid(),
+                name: faker.name.findName(),
+                phone_num: faker.phone.phoneNumberFormat(), 
+                card_num: faker.payment.creditCardNumber(),
+                card_exp_date:  faker.payment.creditCardExpirationDate()
+            };
+    connection.query('INSERT INTO passenger SET ?', passenger, (err, res) => {
+
+        if(err) throw err;
+
+    console.log('Last insert ID:', res.insertId);
+    });
+}
+  
+
+
+
+
+
+
+
+//------------------------------------------------------------------------
+// ************  @MONGODB   *****************
+//------------------------------------------------------------------------
+//Mongo Connection
+// var MongoClient = require('mongodb').MongoClient;
+// var assert = require('assert');
+// var ObjectId = require('mongodb').ObjectID;
+// const url= 'mongodb://localhost:27017/mongotest';
+
+
+// //Select function
+// var findUsers = function(db, callback) {
+//   var cursor =db.collection('tests').find( );
+//   cursor.each(function(err, doc) {
+//      assert.equal(err, null);
+//      if (doc != null) {
+//         console.dir(doc);
+//      } else {
+//         callback();
+//      }
+//   });
+// };//select close
+
+
+// app.all('/', (req, res) => {
+//     res.send("Hello");
+// });
+
+// //Create Route
+// app.get('/create', (req,res) => {
+//     //return res.send(result);
+    
+//          //Insert function
+//         MongoClient.connect(url, function(err, db) {
+//             assert.equal(null, err);
+//             for(var i=1; i<=20; i++){
+//                 var User = {
+//                     passenger_id: faker.uuid(),
+//                     fname: faker.name.firstName(),
+//                     lname: faker.name.lastName(),
+//                     email: faker.internet.email(),
+//                     website: faker.internet.url(),
+//                     address: {
+//                       street: faker.address.streetAddress(),
+//                       city: faker.address.city(),
+//                       country: faker.address.country() 
+//                     },
+//                     bio: faker.lorem.sentences(),
+//                     image: faker.image.avatar()
+//                 };
+//                 db.collection('tests').insertOne( User, function(err, result) {
+//                     assert.equal(err, null);
+//                     console.log("Inserted a document into the tests collection.");
+//                 });
+//             }
+//             db.close();
+//         });//insert close
+// });
+    
+
+// //select Route
+// app.get('/users', (req, res) => {
+//     MongoClient.connect(url, function(err, db) {
+//         assert.equal(null, err);
+//         findUsers(db, function() {
+//             db.close();
+//         });
+//     });
+// })
+
+//-----------------------------------------------------------------------
+// MONGODB CLOSE
+//---------------------------------------------------------------------------
+
+
+
 
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'))
